c865cb0cdd56b6d3f42bace9868be8c0af4ddb0e
Gehrig Dixon <gmand4101@gmail.com>
2023-12-04 17:38:01 -0700

    modified:   user/games.nix

--- user/games.nix
+++ user/games.nix
@@ -3,5 +3,14 @@
   users.users.${username}.packages = with pkgs;[
     retroarch
     minecraft
-  ]
+    ryujinx
+    dolphin-emu
+    cemu
+  ];
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+  hardware.opengl.driSupport32Bit = true;
 }
