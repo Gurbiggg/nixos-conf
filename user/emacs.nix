be7e8e721f646b36fe6a34afc03db30148adbd9a
Gehrig Dixon <gmand4101@gmail.com>
2023-12-23 15:32:27 -0700

    modified:   user/emacs.nix

--- user/emacs.nix
+++ user/emacs.nix
@@ -4,6 +4,29 @@
     enable = true;
     package = pkgs.emacs;
     extraConfig = ''
+      (require 'package)
+      (add-to-list  'package-archives
+                    '("melpa" . "https://melpa.org/packages/"))
+      (package-initialize)
+      (package-refresh-contents)
+
+      (unless (package-installed-p 'use-package)
+        (packages-refresh-contents)
+        (package-install 'use-package))
+
+      (unless (package-installed-p 'evil)
+        (package-install 'evil))
+
+      (require 'evil)
+      (evil-mode 1)
+
+      (use-package timu-rouge-theme
+        :ensure t
+        :config
+        (load-theme 'timu-rouge t ))
+      
+      (customize-set-variable 'timu-rouge-org-intense-colors t)
+      (customize-set-variable 'timu-rouge-mode-line-border t)
       (setq standard-indent 2)
     '';
   };
